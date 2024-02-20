a99603523ccb49bebd9cde8419f34babc6bc8973
Volodimyr213 <Terminal213@gmail.com>
2024-02-20 15:07:38 +0200

    houm-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,35 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const ulHtml = document.querySelector(".gallery");
+// ulHtml.addEventListener("click",startModal);
+
+function makeGalery(elems){
+
+  return elems.map( ({preview,original,description}) => 
+   `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+     <img
+       class="gallery__image"
+       src="${preview}"
+       data-source="large-image.jpg"
+       alt="${description}"
+     />
+   </a>
+ </li>`
+   ).join("")
+
+};
+ulHtml.insertAdjacentHTML('beforeend',makeGalery(galleryItems))
+
+// ====================Marcup====================
+
+ulHtml.addEventListener('click', makeModal)
+
+function makeModal(evt) {
+    evt.preventDefault()
+    if(!evt.target.classList.contains("gallery__image")){
+          return
+    }
+    console.log(evt.target.dataset.source)
+};
